7349247c98c1fb10c92fe0664111c30b7f5b0d78
Gribickis <150134540+Gribickis@users.noreply.github.com>
2024-03-26 12:42:43 +0200

    s

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Sveikiaaaa!"
+    return render_template("index.html")
+
+@app.route("/tests")
+def te():
+    return render_template("tests.html")
+
+
 if __name__ == '__main__':
-    app.run(port = 5000)
+      app.run(port = 5000)
