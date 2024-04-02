eef43b47d3f3862a04aa281aeae3868f8c823d5c
Gribickis <150134540+Gribickis@users.noreply.github.com>
2024-04-02 14:19:20 +0300

    asas

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def index():
 def te():
     return render_template("tests.html")
 
+@app.route("/saraksts")
+def saraksts():
+    saraksts = ["kakis", "katla", "panna"]
+    bildes = ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/1200px-Felis_catus-cat_on_snow.jpg", "https://arkolat.lv/storage/uploads/global/product_images/image/000/055/684/large/ef387b5ad94e9169c7f220dda11f92da.jpg", "https://ksd-images.lt/display/aikido/store/9e14ae8a2575d2ffabdc0c6476c47f3d.jpg"]
+    return render_template("saraksts.html", vardi = saraksts, bildes = bildes)
 
 if __name__ == '__main__':
       app.run(port = 5000)
